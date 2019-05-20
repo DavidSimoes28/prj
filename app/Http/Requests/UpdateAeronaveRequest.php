09c99d7fdfc1038b31ad533c1adb26903e8dba50
zawek96 <49598175+zawek96@users.noreply.github.com>
2019-05-21 00:29:01 +0100

    Update UpdateAeronaveRequest.php

--- app/Http/Requests/UpdateAeronaveRequest.php
+++ app/Http/Requests/UpdateAeronaveRequest.php
@@ -28,7 +28,7 @@ class UpdateAeronaveRequest extends FormRequest
         $aeronave = $this->route('aeronave');
 
         return [
-            'matricula' => 'required|string|min:1|max:8', //Rule::unique('aeronaves')->ignore($aeronave->matricula)],
+            'matricula' => ['required', 'string', 'min:1', 'max:8', 'unique:aeronaves'], //,matricula,'.$aeronave->matricula],     //Rule::unique('aeronaves')->ignore($aeronave->matricula)],           
             'marca' => 'required|string|min:1|max:40',
             'modelo' => 'required|min:1|max:40',
             'num_lugares' => 'required|integer|min:1|max:999 999 999 99',
