da8c81ff7796c307f2e326ae38c4c758b7d301d8
zawek96 <zawek96@gmail.com>
2019-05-23 17:09:41 +0100

    mais mudancas na vista movimentos direcao

--- resources/views/movimentos/direcao/editDirecao.blade.php
+++ resources/views/movimentos/direcao/editDirecao.blade.php
@@ -25,5 +25,22 @@
     </div>
 </div>
 
+<div class="form-group row">
+    <label for ="confirmado" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de Instrução') }}</label>
+        <div class="col-sm-6">
+            <select class="btn btn-xs btn-primary dropdown-toggle btn-block {{ $errors->has('confirmado') ? ' is-invalid' : '' }}"  name="confirmado" value="{{ strval(old('confirmado',request()->confirmado )) }}" >
+            <option value="0"  {{ strval(old('confirmado' ,request()->confirmado)) == "0"  ? "selected":"" }} >Não Confirmado</option>
+            <option value="1"  {{ strval(old('confirmado' ,request()->confirmado)) == "1"      ? "selected":"" }} >Confirmado</option>
+            
+            </select> 
+            
+            @if ($errors->has('confirmado'))
+                <span class="invalid-feedback" role="alert">
+                    {{ $errors->first('confirmado') }}
+                </span>
+            @endif   
+        </div>                     
+</div>
+
 
 <!--   FALTA A CONFIRMAÇÃO DO VOO   -->
