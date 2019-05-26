05cb391555f21c556e3c06eff5b246a733d80d10
zawek96 <49598175+zawek96@users.noreply.github.com>
2019-05-26 17:39:54 +0100

    US30

--- app/Http/Controllers/PendentesController.php
+++ app/Http/Controllers/PendentesController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Movimento;
 use Illuminate\Http\Request;
 
 class PendentesController extends Controller
 {
     public function index(){
-        $pendentes = Movimento::where('confirmado','0')->paginate(5);
-        return view('pendentes.listPendentes', compact('pendentes'));
+        $movimentos = Movimento::where('confirmado','0')->get();
+
+        $licencas = User::where('tipo_socio','P')
+        ->whereNotNull('num_licenca')
+        ->whereNull('licenca_confirmada')
+        ->get();
+
+        $certificados = User::where('tipo_socio','P')
+        ->whereNotNull('num_certificado')
+        ->whereNull('certificado_confirmado')
+        ->get();
+
+        $conflitos=array();
+
+        return view('pendentes.listPendentes', compact('movimentos','certificados','licencas','conflitos'));
     }
     
 }
