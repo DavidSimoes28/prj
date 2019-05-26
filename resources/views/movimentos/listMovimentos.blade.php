0ad06340926188e9865be4e4da1507811f9f3555
zawek96 <49598175+zawek96@users.noreply.github.com>
2019-05-26 17:46:13 +0100

    modal confirmacao

--- resources/views/movimentos/listMovimentos.blade.php
+++ resources/views/movimentos/listMovimentos.blade.php
@@ -117,42 +117,8 @@
                         <a class="btn btn-xs btn-primary" href="{{ route('movimentos.create') }}" data-toggle="tooltip" title="Adicionar Movimento">{{ __('Adicionar Movimento') }}</a>
                             <div class="dropdown-divider"></div>   
                             @if(Auth::user()->isAdmin())
-                            
-                            <li>
-                                </li>
                                 
-                                    <a href="#list_confirmacao" class="btn btn-success btn-block" data-toggle="modal" data-target="#list_confirmacao" >{{ __('Confirmar') }}</a> 
-                                    <button type="button" class="btn btn-success btn-block" data-toggle="modal" data-target="#list_confirmacao">
-                                            Guardar
-                                    </button>
-                                    <li>
-                                    </li>
-                                
-                                    <div class="modal fade" id="list_confirmacao" tabindex="-1" role="dialog" aria-labelledby="list_confirmacaoLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="list_confirmacaoLabel">Modal title</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                ...
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-primary">Save changes</button>
-
-                                                    
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <li>
-                                        </li>
+                                    <a href="#list_confirmacao" class="btn btn-success btn-block" data-toggle="modal" data-target="#list_confirmacao" >{{ __('Confirmar') }}</a>
                             @endif
                         </div>               
                     @endif
@@ -320,6 +286,28 @@
                                     </td>
                                 </tr>
                             @endforeach
+                            <div class="modal" id="list_confirmacao" tabindex="-1" role="dialog" aria-labelledby="list_confirmacaoLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="list_confirmacaoLabel">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                ...
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Save changes</button>
+
+                                                    
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                         </tbody>
                         </table>
                         {{ $movimentos->links() }}
